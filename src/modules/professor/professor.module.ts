636f3f7e1934f48347ae8eda7974a60e33f583cd
Anderson Freires de Freitas <andffreires@gmail.com>
2025-10-15 11:09:09 -0300

    update module to bind port to adapter

--- src/modules/professor/professor.module.ts
+++ src/modules/professor/professor.module.ts
@@ -2,26 +2,34 @@ import { Module } from "@nestjs/common";
 import { DatabaseModule } from "../database/database.module";
 import { GetProfessorRepository } from "./repositories/getProfessor.repository";
 import { ProfessorController } from "./controller/professor.controller";
-import { GetProfessorService } from "./services/getProfessor.service";
 import { PostProfessorRepository } from "./repositories/postProfessor.repository";
-import { PostProfessorService } from "./services/postProfessor.service";
 import { PutProfessorRepository } from "./repositories/putProfessor.repository";
-import { PutProfessorService } from "./services/putProfessor.service";
 import { DeleteProfessorRepository } from "./repositories/deleteProfessor.repository";
+import { GetProfessorService } from "./services/getProfessor.service";
+import { PostProfessorService } from "./services/postProfessor.service";
+import { PutProfessorService } from "./services/putProfessor.service";
 import { DeleteProfessorService } from "./services/deleteProfessor.service";
+import { ProfessorRepositoryAdapter } from "./infrastructure/repositories/professor.repository.adapter";
+import { ProfessorRepositoryPortToken } from "./application/ports/professor-repository.port";
+
 
 @Module({
   imports: [DatabaseModule],
   controllers: [ProfessorController],
   providers: [
-    PostProfessorRepository,
-    PostProfessorService,
     GetProfessorRepository,
-    GetProfessorService,
+    PostProfessorRepository,
     PutProfessorRepository,
-    PutProfessorService,
     DeleteProfessorRepository,
+    GetProfessorService,
+    PostProfessorService,
+    PutProfessorService,
     DeleteProfessorService,
+
+    {
+      provide: ProfessorRepositoryPortToken,
+      useClass: ProfessorRepositoryAdapter,
+    },
   ],
 })
 export class ProfessorModule {}
