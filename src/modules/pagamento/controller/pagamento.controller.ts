01b84d11b410a71e75d1d6e37852ab99ebddfd94
Anderson Freires de Freitas <anderson.freitas@livsaude.com.br>
2025-10-22 16:33:20 -0300

    feat: creating postPagamento controller method

--- src/modules/pagamento/controller/pagamento.controller.ts
+++ src/modules/pagamento/controller/pagamento.controller.ts
@@ -1,21 +1,33 @@
-import { Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { PostCustomerAsaasService } from "../services/postCustomerAsaas.service";
 import { TokenPayload } from "src/modules/auth/interfaces/auth.interface";
 import { User } from "src/common/decorators/user.decorator";
 import { JwtAuthGuard } from "src/common/guards/jwt-auth.guard";
 import { Roles } from "src/common/decorators/role.decorator";
 import { Role } from "src/common/enum/role.enum";
+import { PostPagamentoAsaasDataDTO } from "../dtos/postPagamentoAsaasData.dto";
+import { PostPagamentoAsaasService } from "../services/postPagamentoAsaas.service";
 
 @Controller("pagamento")
 @UseGuards(JwtAuthGuard)
 export class PagamentoController {
   constructor(
-    private readonly postCustomerAsaasService: PostCustomerAsaasService
+    private readonly postCustomerAsaasService: PostCustomerAsaasService,
+    private readonly postPagamentoAsaasService: PostPagamentoAsaasService
   ) {}
 
   @Post("cliente")
-  @Roles(Role.ADM)
+  @Roles(Role.ALUNO, Role.PROFESSOR, Role.ADM)
   async postCliente(@User() user: TokenPayload) {
     return await this.postCustomerAsaasService.execute(user.id_usuario);
+  }
+
+  @Post("gerar")
+  @Roles(Role.ALUNO, Role.PROFESSOR, Role.ADM)
+  async gerarPagamento(
+    @User() user: TokenPayload,
+    @Body() data: PostPagamentoAsaasDataDTO
+  ) {
+    return await this.postPagamentoAsaasService.execute(user.id_usuario, data);
   }
 }
--- src/modules/pagamento/pagamento.module.ts
+++ src/modules/pagamento/pagamento.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PagamentoController } from "./controller/pagamento.controller";
 import { PostCustomerAsaasService } from "./services/postCustomerAsaas.service";
+import { PostPagamentoAsaasService } from "./services/postPagamentoAsaas.service"; // 👈 novo service
 import { PagamentoRepositoryAdapter } from "./adapters/repositories/pagamento.repository.adapter";
 import { PagamentoRepositoryPortToken } from "./application/ports/pagamento-repository.port";
 import { PostCustomerAsaasRepository } from "./repositories/postCustomerAsaas.repository";
+import { PostPagamentoAsaasRepository } from "./repositories/postPagamentoAsaas.repository"; // 👈 novo repo
 import { DatabaseModule } from "../database/database.module";
 
 @Module({
@@ -11,7 +13,9 @@ import { DatabaseModule } from "../database/database.module";
   controllers: [PagamentoController],
   providers: [
     PostCustomerAsaasService,
+    PostPagamentoAsaasService,
     PostCustomerAsaasRepository,
+    PostPagamentoAsaasRepository,
     {
       provide: PagamentoRepositoryPortToken,
       useClass: PagamentoRepositoryAdapter,
