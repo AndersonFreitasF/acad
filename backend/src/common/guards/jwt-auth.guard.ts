14b32154d7783ae0d0b1510dcb5b4dec2d2965d9
AndersonFreitasF <andffreires@gmail.com>
2025-10-30 09:03:44 -0300

    common: add @Public decorator and update JwtAuthGuard to skip public routes

--- backend/src/common/guards/jwt-auth.guard.ts
+++ backend/src/common/guards/jwt-auth.guard.ts
@@ -5,18 +5,31 @@ import {
   UnauthorizedException,
   ForbiddenException,
 } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import { JwtService } from "@nestjs/jwt";
 import { Request } from "express";
 import { TokenPayload } from "src/modules/auth/interfaces/auth.interface";
 
 import "reflect-metadata";
 import { checkRoles } from "./role.guard";
+import { IS_PUBLIC_KEY } from "../decorators/public.decorator";
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest<Request>();
     const token = this.extractTokenFromHeader(request);
 
