e3cede8964a24def416c07589fb6358797171432
AndersonFreitasF <andffreires@gmail.com>
2025-10-30 10:54:42 -0300

    dasd

--- backend/src/modules/professor/services/postProfessor.service.ts
+++ backend/src/modules/professor/services/postProfessor.service.ts
@@ -17,7 +17,7 @@ export class PostProfessorService {
 
   async execute(data: PostProfessorDataDTO, created_by: number) {
     try {
-      await this.repo.postProfessor(
+      await this.repo.postUsuario(
         { ...data, senha: await this.passwordHasher.hash(data.senha) },
         created_by
       );
